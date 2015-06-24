a7821e1a08ef4655616e63b83bf27b838fa1be76
Aaron Bowers <aarbow@hotmail.com>
2015-06-24 14:08:03 +0000

    All  done... see page 109 to continue.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,21 @@
 node 'cookbook' {
-  include puppet
-  include memcached
-  include packages
+#  append_if_no_such_line { 'enable-ip-conntrack':
+#    file => '/etc/modules',
+#    line => 'ip_conntrack',
+#  }
+#  include puppet
+#  include memcached
+#  include packages
+#  $mysql_password = 'secret'
+#  file { '/usr/local/bin/backup-mysql':
+#    content => template('admin/backup-mysql.sh.erb'),
+#    mode    => '0755',
+#  }
+  $ipaddresses = ['192.168.0.1',
+                  '158.43.128.1',
+                  '10.0.75.207', ]
+
+  file { '/tmp/addresslist.txt':
+    content => template('admin/addresslist.erb')
+  }
 }
